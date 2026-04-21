359feeb1749205d5b0c540c211d0e0a70bbf2614
Hüseyin Ahmet Altun <huseyinahmetaltun@gmail.com>
2026-04-21 18:39:24 +0300

    Add files via upload

---
+++ video_1.py
@@ -0,0 +1,12 @@
+# Karar yapıları
+# if
+# elif
+# else
+
+x = int(input("Lütfen bir sayı giriniz: "))
+if x > 0:
+    print("Sayı pozitiftir.")
+elif x < 0:
+    print("Sayı negatiftir.")
+else:
+    print("Sayı sıfırdır.")
